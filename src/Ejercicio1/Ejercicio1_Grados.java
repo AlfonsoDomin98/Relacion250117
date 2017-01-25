e41040ef13bd29b34ea88fe6f963ce86c6631939
Alfonso <Alfonso@192.168.1.196>
2017-01-25 13:02:13 +0100

    Ejercicio 1 hecho

--- src/Ejercicio1/Ejercicio1_Grados.java
+++ src/Ejercicio1/Ejercicio1_Grados.java
@@ -1,10 +1,41 @@
 package Ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio1_Grados {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Introduccion de Variables
+		Scanner teclado = new Scanner (System.in);
+		float grados = 0;
+		float salida = 0;
+		boolean esCorrecto = false;
+		
+		// Validacion de los datos
+		do{
+			System.out.println("Introduzca la cantidad de Grados: ");
+			try{
+				grados = teclado.nextFloat();
+				esCorrecto = true;
+			}
+			catch(Exception e){
+				System.out.println("ERROR: No ha introducido un dato correcto");
+				esCorrecto = false;
+			}
+		}
+		while (esCorrecto == false);
+		
+		salida = Grados(grados);
+		
+		System.out.println("Son "+salida+ " Fah");
+		
 	}
-
+	public static float Grados (float conversion){
+		float grados = 0;
+		
+		grados = 32 + ( 9 * conversion / 5);
+		
+		return grados;
+	}
+	
 }
